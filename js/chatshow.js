89ebde2c245da3ae3050945e0eec6ccc065df562
Janis7386 <ms-technician-7386@outlook.com>
2026-02-05 14:57:40 -0600

    Add files via upload

--- js/chatshow.js
+++ js/chatshow.js
@@ -1,8 +1,11 @@
+let tidioListenerAdded = false;
+
 function chatShow() {
     if (window.tidioChatApi) {
         window.tidioChatApi.show();
         window.tidioChatApi.open();
-    } else {
+    } else if (!tidioListenerAdded) {
+        tidioListenerAdded = true;
         document.addEventListener("tidioChat-ready", function () {
             window.tidioChatApi.show();
             window.tidioChatApi.open();
